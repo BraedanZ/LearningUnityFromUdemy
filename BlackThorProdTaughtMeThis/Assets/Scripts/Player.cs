a7bcbcd8557e5bb935863cca160027e1c6e2f06d
BraedanZ <60045806+BraedanZ@users.noreply.github.com>
2020-09-29 22:25:23 -0400

    Add already build game to the repo

---
+++ BlackThorProdTaughtMeThis/Assets/Scripts/Player.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+
+    public GameObject losePanel;
+
+    public Text healthDisplay;
+
+    public float speed;
+    public int health;
+    private float input;
+
+    Rigidbody2D rigidBody;
+    Animator animator;
+
+    public float startDashTime;
+    private float dashTime;
+    public float extraSpeed;
+    private bool isDashing;
+
+    // Start is called before the first frame update
+    void Start() {
+        animator = GetComponent<Animator>();
+        rigidBody = GetComponent<Rigidbody2D>();
+        healthDisplay.text = health.ToString();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (input != 0) {
+            animator.SetBool("isRunning", true);
+        } else {
+            animator.SetBool("isRunning", false);
+        }
+
+        if (input < 0) {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        } else if (input > 0) {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isDashing == false) {
+            speed += extraSpeed;
+            isDashing = true;
+            dashTime = startDashTime;
+        }
+        if (dashTime <= 0 && isDashing == true) {
+            isDashing = false;
+            speed -= extraSpeed;
+        } else {
+            dashTime -= Time.deltaTime;
+        }
+    }
+
+    void FixedUpdate() {
+        input = Input.GetAxisRaw("Horizontal");
+
+        rigidBody.velocity = new Vector2(input * speed, rigidBody.velocity.y);
+    }
+    
+    public void TakeDamage(int damageAmount) {
+        health -= damageAmount;
+        healthDisplay.text = health.ToString();
+
+        if (health <= 0) {
+            losePanel.SetActive(true);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ BlackThorProdTaughtMeThis/Assets/Scripts/Spawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject[] hazards;
+
+    public float minTimeBetweenSpawns;
+    public float decrease;
+    public float startTimeBetweenSpawns;
+    private float timeBetweenSpawns;
+    public float startRestTime;
+
+    public GameObject player;
+
+    // Update is called once per frame
+    void Update() {
+        if (startRestTime <= 0) {
+            if (player != null) {
+                if (timeBetweenSpawns <= 0) {
+                    Transform randomSpawnPoint1 = spawnPoints[Random.Range(0, spawnPoints.Length)];
+                    Transform randomSpawnPoint2 = spawnPoints[Random.Range(0, spawnPoints.Length)];
+                    GameObject randomHazard1 = hazards[Random.Range(0, hazards.Length)];
+                    GameObject randomHazard2 = hazards[Random.Range(0, hazards.Length)];
+                    int doubleSpawn = Random.Range(0, 2);
+
+                    Instantiate(randomHazard1, randomSpawnPoint1.position, Quaternion.identity);
+                    if (randomSpawnPoint1 != randomSpawnPoint2 && doubleSpawn == 1) {
+                        Instantiate(randomHazard2, randomSpawnPoint2.position, Quaternion.identity);
+                    }
+                
+                    if (startTimeBetweenSpawns > minTimeBetweenSpawns) {
+                    startTimeBetweenSpawns -= decrease;
+                    }
+                    timeBetweenSpawns = startTimeBetweenSpawns;
+                } else {
+                    timeBetweenSpawns -= Time.deltaTime;
+                }
+            }
+        } else startRestTime -= Time.deltaTime;
+    }
+}
